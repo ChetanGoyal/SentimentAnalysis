8413c51b16ed400d8863365daf4b95eac0398c2d
chetangoyal <er.chetangoyal@gmail.com>
2018-07-11 21:12:35 +0530

    Changes in Code

--- collect.py
+++ collect.py
@@ -15,7 +15,6 @@ def assign_twitter_api():
     consumer_key = ''
     consumer_secret = ''
     access_token = ''
-
     access_token_secret = ''
 
     twitter_api = twitter.Api(consumer_key=consumer_key,
